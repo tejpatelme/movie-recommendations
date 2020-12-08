4eb339651326d6153fee24d9b0ad80586c955270
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2020-12-08 12:28:13 +0530

    add : added movie database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import React from "react";
 import "./styles.css";
 
+var movieDB = {
+  Drama: [
+    {
+      title: "The Shawshank Redemption",
+      cast: "Tim Robbins, Morgan Freeman, Bob Gunton",
+      rating: 9.5 / 10
+    },
+
+    {
+      title: "Good Will Hunting (1997)",
+      cast: "Robin Williams, Matt Damon, Ben Affleck",
+      rating: 9.9 / 10
+    },
+
+    {
+      title: "Forrest Gump (1994)",
+      cast: "Tom Hanks, Robin Wright, Gary Sinise",
+      rating: 9.7 / 10
+    }
+  ],
+
+  Biography: [
+    {
+      title: "Catch Me If You Can",
+      cast: "Leonardo DiCaprio, Tom Hanks, Christopher Walken",
+      rating: 9.5 / 10
+    },
+
+    {
+      title: "The Imitation Game",
+      cast: "Benedict Cumberbatch, Keira Knightley, Matthew Goode",
+      rating: 8.3 / 10
+    },
+
+    {
+      title: "Lone Survivor",
+      cast: "Mark Wahlberg, Taylor Kitsch, Emile Hirsch",
+      rating: 8.6 / 10
+    }
+  ],
+
+  Mystery: [
+    {
+      title: "Shutter Island",
+      cast: "Leonardo DiCaprio, Emily Mortimer, Mark Ruffalo",
+      rating: 8.5 / 10
+    },
+
+    {
+      title: "The Girl with the Dragon Tattoo",
+      cast: "Daniel Craig, Rooney Mara, Christopher Plummer",
+      rating: 9.0 / 10
+    },
+
+    {
+      title: "The Bourne Identity",
+      cast: "Franka Potente, Matt Damon, Chris Cooper",
+      rating: 8.0 / 10
+    }
+  ]
+};
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>🎥My Movie Recommendations</h1>
     </div>
   );
 }
